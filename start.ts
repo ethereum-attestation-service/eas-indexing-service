a22c028b803bc8cbadb2a2015f38fbdb63a35927
Steve Dakh <slavik0329@gmail.com>
2023-02-25 15:48:12 -0500

    more

--- start.ts
+++ start.ts
@@ -1,8 +1,6 @@
 import { ethers } from "ethers";
 import {
   attestedEventSignature,
-  EASContractAddress,
-  EASSchemaRegistryAddress,
   getAndUpdateLatestAttestationRevocations,
   getAndUpdateLatestAttestations,
   getAndUpdateLatestSchemas,
@@ -13,15 +11,9 @@ import {
   timestampEventSignature,
   updateDbFromRelevantLog,
 } from "./utils";
-import { Eas__factory, EasSchema__factory } from "./types/ethers-contracts";
 
 require("dotenv").config();
-const schemaContract = EasSchema__factory.connect(
-  EASSchemaRegistryAddress,
-  provider
-);
 
-const easContract = Eas__factory.connect(EASContractAddress, provider);
 let running = false;
 
 export async function update() {
@@ -44,28 +36,23 @@ export async function update() {
 async function go() {
   await update();
 
-  // easContract.filters.Attested.
-
   // setInterval(async () => {
   //   await go();
   // }, 5000);
 
-  // const filter: ethers.providers.EventType = {
-  //   topics: [
-  //     [
-  //       ethers.utils.id(registeredEventSignature),
-  //       ethers.utils.id(attestedEventSignature),
-  //       ethers.utils.id(revokedEventSignature),
-  //       ethers.utils.id(timestampEventSignature),
-  //     ],
-  //   ],
-  // };
-  //
-  // provider.on(filter, async (log: ethers.providers.Log) => {
-  //   await updateDbFromRelevantLog(log);
-  // });
+  const filter: ethers.providers.EventType = {
+    topics: [
+      [
+        ethers.utils.id(registeredEventSignature),
+        ethers.utils.id(attestedEventSignature),
+        ethers.utils.id(revokedEventSignature),
+        ethers.utils.id(timestampEventSignature),
+      ],
+    ],
+  };
 
-  easContract.on("Timestamped", async (log: ethers.providers.Log) => {
+  provider.on(filter, async (log: ethers.providers.Log) => {
+    console.log("Got log", log);
     await updateDbFromRelevantLog(log);
   });
 }
