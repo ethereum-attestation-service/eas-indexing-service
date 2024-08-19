6797b01ea53ba187fbcf8d5b64f4b376258ec8e6
Steve Dakh <slavik0329@gmail.com>
2024-08-19 16:13:34 -0400

    Support multiple txids in update script

--- scripts/updateForTxId.ts
+++ scripts/updateForTxId.ts
@@ -1,16 +1,18 @@
 import { updateDbFromEthTransaction } from "../utils";
 
-// This is a command line script that accepts a txid as an argument and updates the database with the
-// attestations/schemas from the transaction
+// This is a command line script that accepts either a single txid or a comma-separated list of txids
+// as an argument and updates the database with the attestations/schemas from the transaction(s)
 
-const txid = process.argv[2];
+const input = process.argv[2];
 
-if (!txid) {
-  console.error("Please provide a transaction id");
+if (!input) {
+  console.error("Please provide a transaction id or a comma-separated list of transaction ids");
   process.exit(1);
 }
 
-updateDbFromEthTransaction(txid)
+const txids = input.split(',').map(txid => txid.trim());
+
+Promise.all(txids.map(updateDbFromEthTransaction))
   .then(() => {
     console.log("Success");
     process.exit(0);
