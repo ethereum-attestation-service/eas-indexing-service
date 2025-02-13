f7b7383a070bdee24294adaa3713eacea0ecfb5a
dzobbe <mazzeo.gio@gmail.com>
2025-02-13 14:19:44 +0100

    commented prisma

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,17 +7,17 @@ DB_PASSWORD=$(echo $DATABASE_URL | cut -d'@' -f1 | cut -d':' -f3 | cut -d'/' -f3
 DB_NAME=$(echo $DATABASE_URL | cut -d'/' -f4)
 
 
-# Wait for PostgreSQL to be available
-until PGPASSWORD=$DB_PASSWORD psql -p 5432 -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
-  echo "PostgreSQL not ready, waiting..."
-  sleep 2
-done
+# # Wait for PostgreSQL to be available
+# until PGPASSWORD=$DB_PASSWORD psql -p 5432 -h "$DB_HOST" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+#   echo "PostgreSQL not ready, waiting..."
+#   sleep 2
+# done
 
-# Run Prisma db push
-npx prisma db push
+# # Run Prisma db push
+# npx prisma db push
 
-# Run Prisma generate
-SKIP_PRISMA_VERSION_CHECK=true npx prisma generate
+# # Run Prisma generate
+# SKIP_PRISMA_VERSION_CHECK=true npx prisma generate
 
 # Start the application
 yarn start
